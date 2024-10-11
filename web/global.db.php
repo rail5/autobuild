027fb0fa34415836103c8c6f8ad6c4dcf1a110ca
rail5 <andrew@rail5.org>
2024-10-11 19:26:13 +0800

    Beginning to add functionality to settings.web.php Added tables to SQLite db for cron jobs Added startup check to verify db structure is up to date

--- web/global.db.php
+++ web/global.db.php
@@ -16,8 +16,34 @@ $db->exec('CREATE TABLE IF NOT EXISTS "data" (
 	"password"	TEXT NOT NULL,
 	"latest_build"	TEXT NOT NULL DEFAULT "Never",
 	"builds"	INTEGER NOT NULL DEFAULT 0,
+	"auto_clear_builds"	INTEGER NOT NULL DEFAULT 0,
+	"auto_clear_builds_minutes"	INTEGER NOT NULL DEFAULT 0,
+	"auto_clear_logs"	INTEGER NOT NULL DEFAULT 0,
+	"auto_clear_logs_minutes"	INTEGER NOT NULL DEFAULT 0,
+	"auto_upgrade_vms"	INTEGER NOT NULL DEFAULT 0,
+	"auto_upgrade_vms_minutes"	INTEGER NOT NULL DEFAULT 0,
 	PRIMARY KEY("uid" AUTOINCREMENT)
 );');
+
+if (!db_structure_is_up_to_date()) {
+	try {
+		$db->exec('BEGIN TRANSACTION;
+		ALTER TABLE "data" ADD COLUMN "uid" INTEGER NOT NULL UNIQUE;
+		ALTER TABLE "data" ADD COLUMN "username" TEXT NOT NULL;
+		ALTER TABLE "data" ADD COLUMN "password" TEXT NOT NULL;
+		ALTER TABLE "data" ADD COLUMN "latest_build" TEXT NOT NULL DEFAULT "Never";
+		ALTER TABLE "data" ADD COLUMN "builds" INTEGER NOT NULL DEFAULT 0;
+		ALTER TABLE "data" ADD COLUMN "auto_clear_builds" INTEGER NOT NULL DEFAULT 0;
+		ALTER TABLE "data" ADD COLUMN "auto_clear_builds_minutes" INTEGER NOT NULL DEFAULT 0;
+		ALTER TABLE "data" ADD COLUMN "auto_clear_logs" INTEGER NOT NULL DEFAULT 0;
+		ALTER TABLE "data" ADD COLUMN "auto_clear_logs_minutes" INTEGER NOT NULL DEFAULT 0;
+		ALTER TABLE "data" ADD COLUMN "auto_upgrade_vms" INTEGER NOT NULL DEFAULT 0;
+		ALTER TABLE "data" ADD COLUMN "auto_upgrade_vms_minutes" INTEGER NOT NULL DEFAULT 0;
+		COMMIT;');
+	} catch (Exception $e) {
+		// Do nothing
+	}
+}
 
 $username = $db->query('SELECT username FROM "data" WHERE uid=1')->fetchArray();
 
@@ -38,6 +64,34 @@ if ($username && !$_SESSION['logged-in'] && basename($_SERVER['PHP_SELF']) != "l
 
 
 /* Database functions */
+function db_structure_is_up_to_date() {
+	global $db;
+	$tables = array(
+		"uid",
+		"username",
+		"password",
+		"latest_build",
+		"builds",
+		"auto_clear_builds",
+		"auto_clear_builds_minutes",
+		"auto_clear_logs",
+		"auto_clear_logs_minutes",
+		"auto_upgrade_vms",
+		"auto_upgrade_vms_minutes"
+	);
+
+	foreach ($tables as $table) {
+		$test_query = "SELECT INSTR(sql, '$table') FROM sqlite_master WHERE type='table' AND name='data'";
+		$test_result = $db->query($test_query)->fetchArray();
+		
+		if ($test_result == 0) {
+			return false;
+		}
+	}
+	
+	return true;
+}
+
 function create_user($username, $password) {
 	global $db;
 	$query = $db->prepare('INSERT INTO data (username, password) VALUES (:user, :pass)');
@@ -67,6 +121,58 @@ function log_in($username, $password) {
 function get_build_stats() {
 	global $db;
 	return $db->query('SELECT latest_build, builds FROM "data" WHERE uid=1')->fetchArray();
+}
+
+function clear_build_stats() {
+	global $db;
+	return $db->query('UPDATE data SET latest_build = "Never", builds = 0 WHERE uid=1')->fetchArray();
+}
+
+function get_cron_settings() {
+	global $db;
+	return $db->query('SELECT auto_clear_builds, auto_clear_builds_minutes, auto_clear_logs, auto_clear_logs_minutes, auto_upgrade_vms, auto_upgrade_vms_minutes FROM "data" WHERE uid=1')->fetchArray();
+}
+
+function update_cron_settings($new_cron) {
+	global $db;
+
+	// Make sure the input values make sense
+	if ($new_cron['auto_clear_builds'] != 1) {
+		$new_cron['auto_clear_builds'] = 0;
+		$new_cron['auto_clear_builds_minutes'] = 0;
+	}
+
+	if ($new_cron['auto_clear_logs'] != 1) {
+		$new_cron['auto_clear_logs'] = 0;
+		$new_cron['auto_clear_logs_minutes'] = 0;
+	}
+
+	if ($new_cron['auto_upgrade_vms'] != 1) {
+		$new_cron['auto_upgrade_vms'] = 0;
+		$new_cron['auto_upgrade_vms_minutes'] = 0;
+	}
+
+	if ($new_cron['auto_clear_builds'] == 1 && $new_cron['auto_clear_builds_minutes'] < 1) {
+		$new_cron['auto_clear_builds_minutes'] = 1;
+	}
+
+	if ($new_cron['auto_clear_logs'] == 1 && $new_cron['auto_clear_logs_minutes'] < 1) {
+		$new_cron['auto_clear_logs_minutes'] = 1;
+	}
+
+	if ($new_cron['auto_upgrade_vms'] == 1 && $new_cron['auto_upgrade_vms_minutes'] < 30) {
+		$new_cron['auto_upgrade_vms_minutes'] = 30;
+	}
+
+	$query = $db->prepare('UPDATE data SET auto_clear_builds = :acb, auto_clear_builds_minutes = :acbm, auto_clear_logs = :acl, auto_clear_logs_minutes = :aclm, auto_upgrade_vms = :auv, auto_upgrade_vms_minutes = :auvm WHERE uid=1');
+	$query->bindValue(':acb', $new_cron['auto_clear_builds']);
+	$query->bindValue(':acbm', $new_cron['auto_clear_builds_minutes']);
+	$query->bindValue(':acl', $new_cron['auto_clear_logs']);
+	$query->bindValue(':aclm', $new_cron['auto_clear_logs_minutes']);
+	$query->bindValue(':auv', $new_cron['auto_upgrade_vms']);
+	$query->bindValue(':auvm', $new_cron['auto_upgrade_vms_minutes']);
+
+	return $query->execute();
 }
 
 function add_build($date) {
--- web/settings.web.php
+++ web/settings.web.php
@@ -10,6 +10,63 @@ require_once "global.php";
 
 /* Autobuild-web Settings */
 
+if (isset($_GET["action"]) && !isset($_GET["error"])) {
+	// Form submitted
+	if (is_array($_GET["action"])) {
+		redirect_and_die("self");
+	}
+
+	switch ($_GET["action"]) {
+		case "clear-builds":
+			run_autobuild("-r all");
+			redirect_and_die("settings.web.php");
+			break;
+		case "clear-logs":
+			delete_all_logs();
+			redirect_and_die("settings.web.php");
+			break;
+		case "clear-stats":
+			clear_build_stats();
+			redirect_and_die("settings.web.php");
+			break;
+		case "update-cron":
+			$auto_clear_builds = isset($_GET["auto-clear-builds"]) ? "1" : "0";
+			$auto_clear_logs = isset($_GET["auto-clear-logs"]) ? "1" : "0";
+			$auto_upgrade_vms = isset($_GET["auto-upgrade-vms"]) ? "1" : "0";
+
+			$auto_clear_builds_minutes = $_GET["auto-clear-builds-length"];
+			$auto_clear_logs_minutes = $_GET["auto-clear-logs-length"];
+			$auto_upgrade_vms_minutes = $_GET["auto-upgrade-vms-cycle"];
+
+			if (!is_numeric($auto_clear_builds_minutes) || !is_numeric($auto_clear_logs_minutes) || !is_numeric($auto_upgrade_vms_minutes)) {
+				redirect_and_die("settings.web.php");
+			}
+
+			$new_cron_settings = array();
+			$new_cron_settings["auto_clear_builds"]			= intval($auto_clear_builds);
+			$new_cron_settings["auto_clear_logs"]			= intval($auto_clear_logs);
+			$new_cron_settings["auto_upgrade_vms"]			= intval($auto_upgrade_vms);
+			$new_cron_settings["auto_clear_builds_minutes"]	= intval($auto_clear_builds_minutes);
+			$new_cron_settings["auto_clear_logs_minutes"]	= intval($auto_clear_logs_minutes);
+			$new_cron_settings["auto_upgrade_vms_minutes"]	= intval($auto_upgrade_vms_minutes);
+
+			update_cron_settings($new_cron_settings);
+
+			redirect_and_die("settings.web.php");
+			break;
+	}
+}
+
+$cron_settings = get_cron_settings();
+
+$auto_clear_builds_checked	= $cron_settings["auto_clear_builds"] == "1" ? " checked" : "";
+$auto_clear_logs_checked	= $cron_settings["auto_clear_logs"] == "1" ? " checked" : "";
+$auto_upgrade_vms_checked	= $cron_settings["auto_upgrade_vms"] == "1" ? " checked" : "";
+
+$auto_clear_builds_minutes	= $cron_settings["auto_clear_builds_minutes"];
+$auto_clear_logs_minutes	= $cron_settings["auto_clear_logs_minutes"];
+$auto_upgrade_vms_minutes	= $cron_settings["auto_upgrade_vms_minutes"];
+
 display_header();
 display_error_message();
 ?>
@@ -30,14 +87,13 @@ display_error_message();
 						<a class="button" href="?action=clear-builds">Clear Builds Folder</a> &nbsp; <a class="button" href="?action=clear-logs">Clear Logs Folder</a> &nbsp; <a class="button" href="?action=clear-stats">Clear Build Statistics</a>
 						</div>
 						<br><br>
-						<h3>Old Builds</h3>
 						<form action="settings.web.php" method="get">
+						<h3>Old Builds</h3>
 							<input type="hidden" name="submitted" value="true">
 							<input type="hidden" name="action" value="update-cron">
-							<li><input type="checkbox" name="auto-clear-builds" id="auto-clear-builds"> <label for="auto-clear-builds">Auto-delete <u>builds</u> older than</label>
-								<input type="number" name="auto-clear-builds-length" class="inline">
+							<li><input type="checkbox" name="auto-clear-builds" id="auto-clear-builds"<?php echo $auto_clear_builds_checked; ?>> <label for="auto-clear-builds">Auto-delete <u>builds</u> older than &nbsp; </label>
+								<input type="number" name="auto-clear-builds-length" class="inline" min="1" value="<?php echo $auto_clear_builds_minutes; ?>">
 								<select name="auto-clear-builds-length-unit" class="inline">
-									<option value="seconds">Seconds</option>
 									<option value="minutes">Minutes</option>
 									<option value="hours">Hours</option>
 									<option value="days">Days</option>
@@ -45,10 +101,19 @@ display_error_message();
 							</li>
 							<br>
 							<h3>Old Logs</h3>
-							<li><input type="checkbox" name="auto-clear-logs" id="auto-clear-logs"> <label for="auto-clear-logs">Auto-delete <u>logs</u> older than</label>
-								<input type="number" name="auto-clear-logs-length" class="inline">
+							<li><input type="checkbox" name="auto-clear-logs" id="auto-clear-logs"<?php echo $auto_clear_logs_checked; ?>> <label for="auto-clear-logs">Auto-delete <u>logs</u> older than &nbsp; </label>
+								<input type="number" name="auto-clear-logs-length" class="inline" min="1" value="<?php echo $auto_clear_logs_minutes; ?>">
 								<select name="auto-clear-logs-length-unit" class="inline">
-									<option value="seconds">Seconds</option>
+									<option value="minutes">Minutes</option>
+									<option value="hours">Hours</option>
+									<option value="days">Days</option>
+								</select>
+							</li>
+							<br><br>
+							<h3>Build Farm</h3>
+							<li><input type="checkbox" name="auto-upgrade-vms" id="auto-upgrade-vms"<?php echo $auto_upgrade_vms_checked; ?>> <label for="auto-upgrade-vms">Auto-upgrade VMs every &nbsp; </label>
+								<input type="number" name="auto-upgrade-vms-cycle" class="inline" min="1" value="<?php echo $auto_upgrade_vms_minutes; ?>">
+								<select name="auto-upgrade-vms-cycle-unit" class="inline">
 									<option value="minutes">Minutes</option>
 									<option value="hours">Hours</option>
 									<option value="days">Days</option>
